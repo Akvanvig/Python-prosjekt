b8c1a2528b929308661914d2a2c4149ab91b6861
akvanvig <akvanv@gmail.com>
2019-01-21 23:42:21 +0100

    Nettverkstilkobling fra Tjener til klient lagt til

--- Media-kontroll Android to Win/Desktop/Mediakontroll_server.py
+++ Media-kontroll Android to Win/Desktop/Mediakontroll_server.py
@@ -9,7 +9,9 @@ pyAutoGui - https://pyautogui.readthedocs.io/en/latest/install.html
 
 #Nettverkskommunikasjon
 import threading
+import queue
 import socket
+import time
 #Mediakontroll
 from pyautogui import press
 import os
@@ -17,8 +19,10 @@ import os
 #Informasjonshenting
 import platform
 
-port = 49433
-addressList = []
+#Port for kommunikasjon, brukes for kommunikasjon i begge retninger
+mPort = 49433
+sPort = 49434
+maksTilkoblinger = 3
 
 hostName = platform.node()
 dataChanged = True
@@ -33,43 +37,82 @@ time = [] #Time object, [current time in ms, total time in ms]
 '''
 def main():
     print(hostName)
+    #Oppretter Queue for kommunikasjon mellom Tråder (Brukes for å sende klient-adresser mellom tråder)
+    q = queue.Queue()
+
     #Oppretter server socket som lytter på gitt på fra alle avsendere
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind(('', port))
+    serverSocket.bind(('', mPort))
     serverSocket.listen(5)
 
+    #Oppretter Tråd for å sende data
+    sdt = sendDataThread(q, sPort, maksTilkoblinger)
+    sdt.start()
     while True:
         #Godtar tilkoblinger
         (clientSocket, address) = serverSocket.accept()
         ct = clientThread(clientSocket)
-        addressList.append(address)
-        #Hvis 6 enheter er koblet til tjeneren, vil første fjernes
-        if len(addressList) > 5:
-            addressList.pop(0)
+        q.put(address)
         #Mottar kommando fra klient
         ct.start()
 
-'''
+
 class sendDataThread (threading.Thread):
-    def __init__(self, ip, port, hostName):
-        self.ip = ip
+    def __init__(self, q, port, maksTilkoblinger):
+        threading.Thread.__init__(self)
+        self.q = q
         self.port = port
-        self.hostName = hostName
+        self.maksTilkoblinger = maksTilkoblinger
+        self.addressList = []
+        self.fortsett = True
 
     def run(self):
         print('Kjører send data tråd')
+        while self.fortsett:
+            if self.q:
+                self.addressList = self.leggTilIP( self.addressList, self.q.get() )
+            self.hentData()
+            data = self.pakkData()
+            for ip in self.addressList:
+                self.sendData(ip, data)
+            time.sleep(2)
+
+    #Sjekker om IP som akkurat kontaktet program er lagt inn i
+    def leggTilIP(self, liste, ip):
+        #IP kommer inn som "('192.168.1.6', 35024)" (Henter ut bare IP)
+        ip_str = str(ip).split("'")[1]
+        if ip_str not in liste:
+            liste.append(ip_str)
+            if len(liste) > self.maksTilkoblinger:
+                liste.pop(0)
+            print(liste)
+        return liste
+
+    #Setter sammen data til en enkelt streng for overføring
+    def pakkData(self):
         info = ''
-        if self.hostName != '':
-            info +='host;;' + self.hostName
+        info +='host;;' + self.hostName
+        return info
+
+    #Sender data på port sPort til alle registrerte klienter (opptil fem)
+    def sendData(self, ip, data):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(self.ip, self.port)
         try:
-            pass
+            s.connect((ip, self.port))
+            byt = data.encode()
+            s.send(byt)
         except Exception as e:
-            raise
-        s.send(info)
+            print('Kunne ikke sende melding til "' + ip + '"')
+            print(e)
         s.close()
-'''
+
+    #Henter inn data til overføring (Kan kobles sammen med API fra forskjellige medieavspillingsprogram som Google Play Music Desktop Player, eller Spotify)
+    def hentData(self):
+        self.hostName = platform.node()
+        return
+
+
+
 class clientThread (threading.Thread):
     def __init__(self, clientSocket):
         threading.Thread.__init__(self)
