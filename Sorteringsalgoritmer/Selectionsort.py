6eb5ee43d7fb95e56deeb70c4505b51c16d189e2
Anders K <akvanv@gmail.com>
2019-06-21 22:25:44 +0200

    .

--- Sorteringsalgoritmer/Selectionsort.py
+++ Sorteringsalgoritmer/Selectionsort.py
@@ -1,8 +1,8 @@
 liste = [0, 5, 32, 1, 3, 22, 102, -22]
 
-def insertionsort(liste):
+def selectionsort(liste):
     lengde = len(liste)
-    min = 0                                 #minimumsverdi funnet i denne gjennomgangen
+    min = 0                                 #minimumsverdi funnet i gjeldende gjennomgangen
     for x in range(0, lengde - 1):
         min = x
         for y in range(x + 1, lengde):
@@ -13,5 +13,5 @@ def insertionsort(liste):
     return liste
 
 print(liste)
-liste = insertionsort(liste)
+liste = selectionsort(liste)
 print(liste)
